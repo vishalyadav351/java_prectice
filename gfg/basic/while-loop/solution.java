68f8f767fa9d0176df9f543d464c34a733f9c8eb
Vishal Yadav <vishuydv991@gmail.com>
2026-07-29 22:02:12 +0530

    ✅ While Loop | Basic | Java | Runtime: N/A | Memory: N/A | 2026-07-29

---
+++ gfg/basic/while-loop/solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public static void utility(int x) {
+        // Continue loop as long as x is greater than or equal to 0
+        while (x >= 0) {
+            System.out.print(x + " ");
+            x--; // Decrement x in each iteration
+        }
+    }
+}
