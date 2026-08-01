556eeba08b1ca385607a52a66049ed334f1e14de
Vishal Yadav <vishuydv991@gmail.com>
2026-08-01 15:51:00 +0530

    ✅ FLOW001 - Rating 242 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-01

---
+++ codechef/easy/FLOW001---Rating-242/solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0)
+		{
+    		int a = sc.nextInt();
+    		int b = sc.nextInt();
+    		System.out.println(a+b);
+		}
+		
+	}
+}
