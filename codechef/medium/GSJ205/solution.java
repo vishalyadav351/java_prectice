5aa9f7c003504c64ee38e0f4a2eef76abc88b1bb
Vishal Yadav <vishuydv991@gmail.com>
2026-08-03 18:04:12 +0530

    ✅ GSJ205 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-03

---
+++ codechef/medium/GSJ205/solution.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        
+        String S = sc.next();
+        
+        System.out.println(N + " " + S);
+        
+        sc.close();
+    }
+}
