15f314810f1aa9ed7d24f743d3b50ad06dcd3baf
Vishal Yadav <vishuydv991@gmail.com>
2026-08-03 18:01:36 +0530

    ✅ GSJ204 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-03

---
+++ codechef/medium/GSJ204/solution.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String A = sc.next();
+        String B = sc.next();
+        
+        String C = sc.next();
+        String D = sc.next();
+        String E = sc.next();
+        
+        System.out.println(A + " " + B + " " + C + " " + D + " " + E);
+        
+        sc.close();
+    }
+}
