107540251ad8e3064c175f68576f636ebc1a022b
Vishal Yadav <vishuydv991@gmail.com>
2026-08-14 10:06:59 +0530

    ✅ Sorted and Rotated Minimum | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-14

---
+++ gfg/easy/minimum-element-in-a-sorted-and-rotated-array3611/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findMin(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+           
+            if (arr[mid] > arr[end]) {
+                start = mid + 1;
+            } 
+            else {
+                end = mid;
+            }
+        }  
+        
+        return arr[start];
+    }   
+}
